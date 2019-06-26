bd8a3f36e8075415e99621751ee4612b001b1de7
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-06-26 19:05:57 +0800

    测试工程搭建

---
+++ spring-boot-tests/spring-boot-goat-tests/spring-boot-goat-mvc-tests/src/main/java/org/springframework/boot/goat/mvc/tests/MyController.java
@@ -0,0 +1,21 @@
+package org.springframework.boot.goat.mvc.tests;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by 64274 on 2019/6/26.
+ *
+ * @ Description: TODO
+ * @ author  山羊来了
+ * @ date 2019/6/26---19:03
+ */
+@RestController
+@RequestMapping("/test")
+public class MyController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world";
+	}
+}
